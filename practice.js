82df4405d99f42a24b88ff771d2a1ac457ddb200
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-07-19 13:40:35 -0500

    first commit

---
+++ practice.js
@@ -0,0 +1,11 @@
+
+
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+    
+
+    return ageInDogYears
+}
+
+const dogAge = calculateAgeInDogYears(45)
+console.log(dogAge)
